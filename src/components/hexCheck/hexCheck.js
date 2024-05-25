9128a4a6afcc4c6c8e4a75352b5ff62404f05c14
Alexander Espinosa Rozov <alexanderespinosarozov@MacBook-Air-Alexander.local>
2024-05-26 00:24:26 +0300

    fix/NRF change to react type style - code

--- src/components/hexCheck/hexCheck.js
+++ src/components/hexCheck/hexCheck.js
@@ -18,18 +18,34 @@ function hexToRgb(hex) {
     }
 
     // check regex
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    if (result) {
-        var rgbText = 'rgb(' + result[1] + ', ' + result[2] + ', ' + + result[3] + ')';
-    }
+    // var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    // if (result) {
+    //     var rgbText = 'rgb(' + result[1] + ', ' + result[2] + ', ' + + result[3] + ')';
+    // }
 
-    return result ? {
-        text: rgbText,
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : false;
+    // return result ? {
+    //     text: rgbText,
+    //     r: parseInt(result[1], 16),
+    //     g: parseInt(result[2], 16),
+    //     b: parseInt(result[3], 16)
+    // } : false;
+
+
+    const result = hex.match(/^([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/i);
+    if (result) {
+      return `rgb(${result.slice(1).map((i) => parseInt(i, 16))})`;
+    }
+  
+    const opt2result = hex.match(/^([0-9a-f])([0-9a-f])([0-9a-f])$/i);
+    if (opt2result) {
+      return `rgb(${opt2result.slice(1).map((i) => 0x11 * parseInt(i, 16))})`;
+    }
+    return false;
+
+
 }
+
+
 
 
 export default hexToRgb
--- src/components/modalColor/modalColor.js
+++ src/components/modalColor/modalColor.js
@@ -1,45 +1,73 @@
 import React from 'react';
 import hexToRgb from '../../components/hexCheck/hexCheck';
+import { useState } from 'react';
 
 
 function ModalColor() {
 
-    const handleSubmit = (event) => {
-        // get on click input the value
-        event.preventDefault();
-        const hex = event.target.value;
+    const [color, setColor] = useState({
+        hex: '',
+        rgb: ''
+    }); // state for color
+    
+    const { hex, rgb } = color;
 
-        const inputRgb = document.querySelector('.input-rgb');
-        const body = document.querySelector('body');
+    const handleChangeHex = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setColor({
+            ...color,
+            [name]: value
+        })
 
-        if (hex.length == 7) {
-            const rgb = hexToRgb(hex);
+        if (value.length == 7) {
+            const rgb_result = hexToRgb(value);
 
-            if (rgb) {
-                inputRgb.value = rgb.text;
-                body.style.backgroundColor = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+            if (rgb_result) {
+                setColor((prevForm) => ({
+                    ...prevForm,
+                    rgb: rgb_result,
+                }));
+
             } else if (rgb === false) {
-                inputRgb.value = 'Ошибка!';
-                body.style.backgroundColor = 'red';
+                setColor((prevForm) => ({
+                    ...prevForm,
+                    rgb: 'Не правильный ввод!',
+                }));
             }
         } else {
-            inputRgb.value = '';
-            body.style.backgroundColor = 'white';
+            setColor((prevForm) => ({
+                ...prevForm,
+                rgb: '',
+            }));
         }
     }
 
-    return (
-        <div>
-            <form className="color-form">
-                <label>
-                    <input className="input-hex" onChange={handleSubmit} type="text" name="hex-color" />
-                </label>
-                <label>
-                    <input className="input-rgb" type="text" readonly="readOnly" name="rgb-color" />
-                </label>
-            </form>
-        </div>
-    );
+
+return (
+    <div
+    className='color-main-body'
+    style={{ backgroundColor: rgb }}>
+        <form className="color-form">
+            <label>
+                <input 
+                className="input-hex" 
+                onChange={handleChangeHex} 
+                type="text" 
+                name="hex" />
+            </label>
+            <label>
+                <input 
+                className="input-rgb" 
+                type="text" 
+                readonly="readOnly" 
+                value={rgb}
+                name="rgb" />
+            </label>
+        </form>
+    </div>
+);
 }
+
 
 export default ModalColor;
